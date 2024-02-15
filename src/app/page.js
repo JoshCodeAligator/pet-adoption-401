7a4c4a5ec5c0e285f0d405b622dd528193ee97fb
joshua girma debele <joshdebele8@gmail.com>
2024-02-15 13:21:45 -0700

    Creating BrowsePetsView

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,12 @@ import Image from 'next/image'
 import HomeView from './components/Home/HomeView'
 import Navbar from './components/Navbar'
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-cover bg-center" style={{ backgroundImage: `url('/images/background.jpg')` }} >
       <Navbar/>
-      <HomeView />
+      <HomeView/>
     </main>
   )
 }
