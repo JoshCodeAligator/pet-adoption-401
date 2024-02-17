583fb9e277726b868e1619f2bcae35b69d0c768e
joshua girma debele <joshdebele8@gmail.com>
2024-02-17 11:45:09 -0700

    Finished the ViewPet and BrowsePets

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { HomeIcon } from "@heroicons/react/solid";
+"use client"
+import React, { useState } from "react";
+import { HomeIcon, MenuIcon, XIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <nav className="bg-gray-900 py-4 fixed top-0 left-0 w-full z-50 shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
@@ -26,20 +33,35 @@ const Navbar = () => {
             <span>Donate</span>
           </Link>
         </div>
-      </div>
-      <div className="lg:hidden">
-        <div className="flex flex-col items-center text-white mt-4">
-          <Link href="/about" className="my-2 hover:text-gray-300">
-            About
-          </Link>
-          <Link href="/contact" className="my-2 hover:text-gray-300">
-            Contact
-          </Link>
-          <Link href="/" className="my-2 hover:text-gray-300">
-            Donate
-          </Link>
+        <div className="lg:hidden">
+          {isSidebarOpen ? (
+            <XIcon
+              className="h-9 w-9 text-white mr-4 cursor-pointer"
+              onClick={toggleSidebar}
+            />
+          ) : (
+            <MenuIcon
+              className="h-9 w-9 text-white mr-4 cursor-pointer"
+              onClick={toggleSidebar}
+            />
+          )}
         </div>
       </div>
+      {isSidebarOpen && (
+        <div className="lg:hidden">
+          <div className="flex flex-col items-center text-white mt-4">
+            <Link href="/about" className="my-2 hover:text-gray-300">
+              About
+            </Link>
+            <Link href="/contact" className="my-2 hover:text-gray-300">
+              Contact
+            </Link>
+            <Link href="/" className="my-2 hover:text-gray-300">
+              Donate
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
