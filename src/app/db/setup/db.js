2fbc420dae738809867f2d3324642350dda61914
FDIL <fdil.fadilh1@gmail.com>
2024-02-18 17:42:22 -0700

    Setup files to connect to MySQL server
    Files not completed.
    Warning, do not commit changed to src/app/db/setup/config.json. That file will have sensitive information about MySQL server.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ src/app/db/setup/db.js
@@ -0,0 +1,50 @@
+// db.js
+const config = require("./config.json"); // Assuming the config file is in the same directory
+
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: config.host_config,
+  user: config.user_config,
+  password: config.password_config,
+  database: config.database_config,
+  connectTimeout: config.connectTimeout_config,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const query = async (sql, values) => {
+  const connection = await pool.getConnection();
+  try {
+    const [rows] = await connection.query(sql, values);
+    return rows;
+  } finally {
+    connection.release();
+  }
+};
+
+const beginTransaction = async () => {
+  const connection = await pool.getConnection();
+  await connection.beginTransaction();
+  connection.release();
+};
+
+const commit = async () => {
+  const connection = await pool.getConnection();
+  await connection.commit();
+  connection.release();
+};
+
+const rollback = async () => {
+  const connection = await pool.getConnection();
+  await connection.rollback();
+  connection.release();
+};
+
+module.exports = {
+  query,
+  beginTransaction,
+  commit,
+  rollback,
+};
