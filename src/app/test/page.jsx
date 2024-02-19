a4ffcd2296577ed099c695daecd66a4206875774
FDIL <fdil.fadilh1@gmail.com>
2024-02-19 14:32:45 -0700

    Got querying db to work.
    test page works. Was able to query all Pets from db and display it.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ src/app/test/page.jsx
@@ -0,0 +1,13 @@
+import {APITest} from "@/app/test/APITest";
+
+
+const page = () => {
+
+    return (
+        <div>
+            <APITest className="absolute top-10 left-10 right-0 bottom-0"/>
+        </div>
+    )
+}
+
+export default page;
