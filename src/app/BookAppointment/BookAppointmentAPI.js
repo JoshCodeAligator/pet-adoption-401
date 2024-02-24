cc5f45195b3a3545492849675bd8d7e4a4133b37
FDIL <fdil.fadilh1@gmail.com>
2024-02-24 15:55:28 -0700

    Small amount of refactoring + added guest flag to Client in db.
    Small amount of refactoring, nothing that can be easily visible at first sight for BookAppointment.
    
    Added guest to Client table in db.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ src/app/BookAppointment/BookAppointmentAPI.js
@@ -0,0 +1,11 @@
+"use server"
+
+// Need a function for possible times
+// db only stores booked times
+
+
+
+
+
+// a function to make a new appointment
+// possibly a new client (if client is a guest)
--- src/app/BookAppointment/page.js
+++ src/app/BookAppointment/page.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import BookingAppointmentController from "@/app/BookAppointment/BookAppointmentController";
 
-const Booking = () => {
-  return <Navbar />;
+const Booking = ({params}) => {
+    return (
+		<BookingAppointmentController pet_id={params.id}/>
+	)
 };
 
 export default Booking;
