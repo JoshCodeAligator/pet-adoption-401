aef126d9889c00309b512261d2cd40d35f78f5b6
FDIL <fdil.fadilh1@gmail.com>
2024-02-24 22:28:42 -0700

    Made LoginAPI.js
    Also experimented adding mantine support as it makes forms easier. However, it messes with the theme made with tailwindCSS. So will not be adding that in (some of the changes made was from that testing).
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ src/app/components/LoginForm.jsx
@@ -0,0 +1,51 @@
+// "use client"
+//
+// import {hasLength, isEmail, useForm} from "@mantine/form";
+// import {Button, PasswordInput, TextInput} from "@mantine/core";
+//
+// const LoginForm = ({onSubmit, loginErrorFlag, loginErrorMessage}) => {
+//
+// 	const form = useForm({
+// 		initialValues: {
+// 			email: '',
+// 			***REMOVED***: '',
+// 		},
+// 		validate: {
+// 			email: isEmail('Invalid email'),
+// 			***REMOVED***: hasLength({min: 1, max: 50}, 'Password must be 1-50 characters')
+// 		},
+// 	})
+//
+//   	return (
+// 		  // <h1>Login Test!</h1>
+// 		<>
+// 			{/* Log in form */}
+// 			<form onSubmit={onSubmit}>
+// 				<TextInput
+// 					label="Email"
+// 					placeholder="your@email.com"
+// 					{...form.getInputProps('email')}
+// 				/>
+// 				<PasswordInput
+// 					label="Password"
+// 					placeholder="***REMOVED***"
+// 					{...form.getInputProps('email')}
+// 				/>
+// 				<Button type="submit">
+// 					Log in
+// 				</Button>
+// 			</form>
+//
+// 			{/* Display error if there is an error */}
+// 			{loginErrorFlag &&
+// 				<p className="accent-red-600">
+// 					{loginErrorMessage}
+// 				</p>
+// 			}
+//
+//
+// 		</>
+// 	)
+// }
+//
+// export default LoginForm
