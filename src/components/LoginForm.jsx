90f62232d9b991ae0d9a4605bb48d735af122e4b
FDIL <fdil.fadilh1@gmail.com>
2024-02-25 12:51:41 -0700

    Refactored Components
    Moved components folder outside of app.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,28 +1,23 @@
 "use client"
 
 import Navbar from "@components/Navbar";
+import FormInput from "@components/FormInput";
 
 const LoginForm = ({onSubmit, loginErrorFlag, loginErrorMessage}) => {
 
 	// need to be to validate values of email and password
-	const submit = (formData) => {
-		alert(`Submit: ${formData.get('email')}, ${formData.get('password')}`)
-	}
 
   	return (
 		<>
 			<Navbar/>
 			{/* Log in form */}
-			<form action={submit} className="absolute top-20">
+			<form action={onSubmit} className="absolute top-20">
 				{/* Email field */}
-				<label htmlFor="email">Enter your email:</label>
-				<input type="email" name="email"/>
-				<br/>
+				<FormInput type="email" name="email" label="Enter your email:"
+						   placeholder="PetPursuit@email.com"/>
 
 				{/* password field */}
-				<label htmlFor="password">Enter your password:</label>
-				<input type="password" name="password"/>
-				<br/>
+				<FormInput type="password" name="password" label="Enter your password:"/>
 
 				{/* Login button */}
 				<button type="submit">Log In</button>
