a598621d0a8a2f2fe0d36c88f1b1a897ffa808dd
FDIL <fdil.fadilh1@gmail.com>
2024-03-02 23:16:37 -0700

    Refactoring of constants.js and started CreateAccount page
    Moved constants.js outside global folder as plan to have all constants in file, so no need for folder.
    
    Got url for CreateAccount setup for separate pages and URL for Client and Admin.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,6 +1,6 @@
 // constants.js
 
-// file with global constants that is used within files
+// file with global constants that is used within the pages of this project
 
 export const category = "category"
 
@@ -9,3 +9,5 @@ export const dog = "Dogs"
 export const cat = "Cats"
 export const rabbit = "Rabbits"
 export const exotic = "Exotics"
+export const admin = "admin"
+export const client = "client"
