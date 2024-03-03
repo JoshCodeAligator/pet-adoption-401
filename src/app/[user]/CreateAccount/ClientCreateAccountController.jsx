32e16770e898765de5c26f2d001bb3640c4bd6c5
FDIL <fdil.fadilh1@gmail.com>
2024-03-03 13:20:45 -0700

    Setup verification logic for CreateClientAccountForm.jsx
    Setup verification code structure. Just now need API functions to be called and error setting and redirect.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/[user]/CreateAccount/ClientCreateAccountController.jsx
+++ src/app/[user]/CreateAccount/ClientCreateAccountController.jsx
@@ -1,8 +1,23 @@
 import ClientCreateAccountView from "@CreateAccount/ClientCreateAccountView";
+import {useState} from "react";
 
 const ClientCreateAccountController = () => {
+	const [errorFlag, setErrorFlag] = useState(false)
+	const [error, setError] = useState('')
+
+
+
+	// function to validate create account form
+	const validateCreateAccountForm = async (formData) => {
+		const firstName = formData.get("first_name")
+		const lastName = formData.get("last_name")
+		const email = formData.get("email")
+		const password = formData.get("password")
+		const confirm_password = formData.get("confirm_password")
+	}
 	return (
-		<ClientCreateAccountView/>
+		<ClientCreateAccountView onSubmit={validateCreateAccountForm}
+								 errorFlag={errorFlag} errorMessage={error}/>
 	)
 }
 
