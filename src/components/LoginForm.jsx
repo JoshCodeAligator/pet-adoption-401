8e4394b73120c3ed20b75c32135fab748b29c69c
FDIL <fdil.fadilh1@gmail.com>
2024-03-03 12:31:41 -0700

    Edit LoginForm and FormInput a bit.
    Edits made were mostly refacotring. Done in prep of another CreateClientAccountForm.jsx
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Navbar from "@components/Navbar";
 import FormInput from "@components/FormInput";
 
 const LoginForm = ({onSubmit, loginErrorFlag, loginErrorMessage}) => {
@@ -9,29 +8,25 @@ const LoginForm = ({onSubmit, loginErrorFlag, loginErrorMessage}) => {
 
   	return (
 		<>
-			<Navbar/>
+			{/* Log in form */}
+			<form action={onSubmit}>
+				{/* Email field */}
+				<FormInput type="email" name="email" label="Enter your email:"
+						   placeholder="PetPursuit@email.com"/>
+				<br/>
+				{/* password field */}
+				<FormInput type="password" name="password" label="Enter your password:"/>
+				<br/>
+				{/* Login button */}
+				<button type="submit">Log In</button>
+			</form>
 
-			<div className="absolute top-20">
-				{/* Log in form */}
-				<form action={onSubmit}>
-					{/* Email field */}
-					<FormInput type="email" name="email" label="Enter your email:"
-							   placeholder="PetPursuit@email.com"/>
-
-					{/* password field */}
-					<FormInput type="password" name="password" label="Enter your password:"/>
-
-					{/* Login button */}
-					<button type="submit">Log In</button>
-				</form>
-
-				{/* Display error if there is an error */}
-				{loginErrorFlag &&
-					<p>
-						{loginErrorMessage}
-					</p>
-				}
-			</div>
+			{/* Display error if there is an error */}
+			{loginErrorFlag &&
+				<p>
+					{loginErrorMessage}
+				</p>
+			}
 
 		</>
 	)
