d2b947382b539eec303d52d112cdeb90b76edb70
FDIL <fdil.fadilh1@gmail.com>
2024-03-03 12:30:55 -0700

    Removed guest column from Client
    Also moved sample data to a separate sql file.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/db/sql/create.sql
+++ src/db/sql/create.sql
@@ -63,7 +63,6 @@ CREATE TABLE Client (
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     account_id INT,
-    guest TINYINT(1) DEFAULT 1,
     PRIMARY KEY(client_id)
 );
 
@@ -127,55 +126,3 @@ CREATE TABLE Appointment (
                 ON UPDATE CASCADE
                 ON DELETE CASCADE
 );
-
--- Now insert some test data
-
--- insert a Center
-INSERT INTO RescueCentre (centre_id, name, address, phone, slot_len_min, num_slot_per_day)
-VALUES (1, 'CentreA', 'Centre A address', '4030804940', 30, 1);
-
--- insert some PetType
-INSERT INTO PetType (type_id, category, breed)
-VALUES (1, 'Dog', 'Breed A');
-
-INSERT INTO PetType (type_id, category, breed)
-VALUES (2, 'Dog', 'Breed B');
-
-INSERT INTO PetType (type_id, category, breed)
-VALUES (3, 'Cat', 'Cat Breed A');
-
-INSERT INTO PetType (type_id, category, breed)
-VALUES (4, 'Cat', 'Cat Breed B');
-
--- insert some Dogs
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Henry', 'M', 4, null, 'A dog', 'available', 1, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Dog B', 'M', 2, null, 'dog b', 'available', 1, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Dog C', 'M', 2, null, 'dog c', 'booked', 2, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Dog D', 'F', 2, null, 'dog d', 'available', 2, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Dog E', 'F', 3, null, 'dog e', 'adopted', 2, 1);
-
-
--- insert some Cats
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Cat A', 'F', 1, null, 'cat a', 'adopted', 3, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Cat B', 'F', 1, null, 'cat b', 'available', 3, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Cat C', 'M', 4, null, 'cat c', 'available', 3, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Cat D', 'M', 2, null, 'cat d', 'available', 4, 1);
-
-INSERT INTO Pet (name, sex, age, img, description, status, type_id, centre_id)
-VALUES ('Cat E', 'M', 3, null, 'cat e', 'available', 4, 1);
