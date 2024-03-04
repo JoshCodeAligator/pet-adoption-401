4b6b7aa39c931e3fbec431beeed571a4b0a68bb6
FDIL <fdil.fadilh1@gmail.com>
2024-03-03 20:38:42 -0700

    Made CreateAccountAPI.js
    Functions not yet tested if actually works.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/[user]/CreateAccount/ClientCreateAccountController.jsx
+++ src/app/[user]/CreateAccount/ClientCreateAccountController.jsx
@@ -2,12 +2,14 @@
 
 import ClientCreateAccountView from "@CreateAccount/ClientCreateAccountView";
 import {useState} from "react";
+import {createClientAccount} from "@CreateAccount/CreateAccountAPI";
+import {useRouter} from "next/navigation";
 
 const ClientCreateAccountController = () => {
 	const [errorFlag, setErrorFlag] = useState(false)
 	const [error, setError] = useState('')
 
-
+	const router = useRouter()
 
 	// function to validate create account form
 	const validateCreateAccountForm = async (formData) => {
@@ -17,6 +19,23 @@ const ClientCreateAccountController = () => {
 		const email = formData.get("email")
 		const ***REMOVED*** = formData.get("***REMOVED***")
 		const confirm_***REMOVED*** = formData.get("confirm_***REMOVED***")
+
+
+		// TODO: field validation
+
+		const result = await createClientAccount(
+			{firstName, lastName, phone, email, ***REMOVED***})
+
+		// Success in client account creation,
+		if (result.success) {
+			// go to login page to login
+			router.push("/login")
+			return
+		}
+
+		setErrorFlag(true)
+		setError(result.error)
+
 	}
 	return (
 		<ClientCreateAccountView onSubmit={validateCreateAccountForm}
