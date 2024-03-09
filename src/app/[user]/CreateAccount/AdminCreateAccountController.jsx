bfa0b490e21fa1fd5227fb1978fb9579dad30306
FDIL <fdil.fadilh1@gmail.com>
2024-03-08 22:43:29 -0700

    Coded backend and validation for create admin account. A bug requires fixing.
    Not extensively tested. A bug exists with getting a runtime error when not selecting a centre.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/[user]/CreateAccount/AdminCreateAccountController.jsx
+++ src/app/[user]/CreateAccount/AdminCreateAccountController.jsx
@@ -3,8 +3,9 @@
 import AdminCreateAccountView from "@CreateAccount/AdminCreateAccountView";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
-import {getAllRescueCentres} from "@CreateAccount/CreateAccountAPI";
+import {createAdminAccount, getAllRescueCentres} from "@CreateAccount/CreateAccountAPI";
 import RescueCentre from "@CreateAccount/RescueCentre";
+import {address, confirm_password, dropDownMenuName, email, password, phone} from "@/app/constants";
 
 export const AdminCreateAccountController = () => {
 	const [errorFlag, setErrorFlag] = useState(false)
@@ -26,12 +27,81 @@ export const AdminCreateAccountController = () => {
 		})
 	}, []);
 
-	const validateAdminForm = () => {
+	const validateAdminForm = ({centre_index, name, address, phone, email, password, confirm_password}) => {
+		// first check all fields filled in
 
+		if (centre_index === -1 && (!name || !address || !phone || !email || !password)) {
+			setErrorFlag(true)
+			setError("Fill all fields")
+			console.log("Did not select a rescue centre, and didn't fill in all fields.")
+			return false
+		}
+		// below is case where select an existing rescue centre, then no need to fill out centre fields
+		else if (centre_index !== -1 && (!email || !password)) {
+			setErrorFlag(true)
+			setError("Fill email and password")
+			console.log("Didn't fill out email or password, but selected a rescue centre")
+			return false
+		}
+
+		// check if confirm password matches
+		if (password !== confirm_password) {
+			setErrorFlag(true)
+			setError("Password and Confirm Password needs to be the same")
+			console.log("Confirm password doesn't match")
+			return false
+		}
+
+		console.log("Validation success")
+		return true
 	}
 
 	const adminFormSubmit = async (formData) => {
+		// clear error
+		setErrorFlag(false)
+		setError("")
+		// might be optional to do as if just change page on success, then multiple attempts are made while flag is true
 
+		const formFields = {
+			centre_index: formData.get(dropDownMenuName),
+			centre_id: -1,	// needs to be set later, isn't actually a form field, but data derived from on
+			// added to this object to make it easier to use later
+			name: formData.get(name),
+			address: formData.get(address),
+			phone: formData.get(phone),
+			email: formData.get(email),
+			password: formData.get(password),
+			confirm_password: formData.get(confirm_password)
+		}
+
+		// set centre_id if it isn't -1
+		if (formFields.centre_index !== -1) {
+			formFields.centre_id = allRescueCentres[formFields.centre_index].id
+		}
+		// issue with if statement above, it seems to trigger even if it should have value -1?
+		// might want to somehow be able to pass up the RescueCentre selected, not just the index
+
+		// use a state passed all the way down from here
+
+		// form seems to work however if actually select an existing centre
+
+		// validate fields
+		if (!validateAdminForm(formFields)) {
+			// failed validation, don't continue
+			return
+		}
+
+		const result = await createAdminAccount(formFields)
+		console.log(result)
+
+		if (result.success) {
+			alert("Admin account created")
+			router.push('/login')
+			return
+		}
+
+		setErrorFlag(true)
+		setError(result.error)
 	}
 
 
