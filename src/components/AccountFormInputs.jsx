e5782d13236ce06dd4db52c9e646b936097ebce2
FDIL <fdil.fadilh1@gmail.com>
2024-03-08 18:20:02 -0700

    Form refactoring
    Refactoring with how forms look like by putting each FormInput.jsx within <div> instead of <> and <br/> in form components.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/components/AccountFormInputs.jsx
+++ src/components/AccountFormInputs.jsx
@@ -3,17 +3,15 @@ import {confirm_password, email, password} from "@/app/constants";
 
 const AccountFormInputs = () => {
 	return (
-		<>
+		<div>
 			{/* email */}
 			<FormInput type={"email"} name={email} label={"Email"}
 					   placeholder={"PetPursuit@email.com"}/>
-			<br/>
 			{/* password */}
 			<FormInput type={"password"} name={password} label={"Password"}/>
-			<br/>
 			{/* confirm password */}
 			<FormInput type={"password"} name={confirm_password} label={"Confirm Password"}/>
-		</>
+		</div>
 	)
 }
 
