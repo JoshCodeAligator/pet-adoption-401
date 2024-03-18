68e69a5c1352a0dfc31710fd9eba5db744cd1a43
joshua girma debele <joshdebele8@gmail.com>
2024-03-18 12:16:25 -0600

    Added BookAppointment Calendar

--- src/app/BookAppointment/page.js
+++ src/app/BookAppointment/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import BookingAppointmentController from "@/app/BookAppointment/BookAppointmentController";
-
 const Booking = ({params}) => {
     return (
 		<BookingAppointmentController pet_id={params.id}/>
