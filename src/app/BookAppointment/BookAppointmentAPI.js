95262b54a7aa874676f3b454bfd47778b0a1e5a7
FDIL <fdil.fadilh1@gmail.com>
2024-03-19 17:44:54 -0600

    Start testing BookAppointmentAPI.js
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/BookAppointment/BookAppointmentAPI.js
+++ src/app/BookAppointment/BookAppointmentAPI.js
@@ -3,9 +3,52 @@
 // Need a function for possible times
 // db only stores booked times
 
+import query from "@/db/setup/db";
+
+export async function getBookedTimesOfWeek(weekStartDate, centreID) {
+
+	// because mySQL BETWEEN is inclusive, only add 6 days to start, (not 7)
+	let weekEndDate = new Date(weekStartDate)
+	weekEndDate.setDate(weekEndDate.getDate() + 6)
 
 
+	const startDate = convertJSDateToMySQLDate(weekStartDate)
+	const endDate = convertJSDateToMySQLDate(weekEndDate)
 
+	console.log(startDate, endDate)
+
+	try {
+		const bookedTimesResult = await query(
+			'SELECT date, start_time FROM Appointment WHERE centre_id = ? AND (date BETWEEN ? AND ?)',
+			[centreID, startDate, endDate]
+		)
+
+		console.log(bookedTimesResult)
+
+		return {
+			success: true,
+			error: "",
+			data: bookedTimesResult
+		}
+	}
+	catch (e) {
+		console.log(e)
+
+		const error = e.error ? e.error : e.message
+
+		return {
+			success: false,
+			error: error,
+			data: []
+		}
+	}
+
+}
+
+// helper function to convert js date objects to MySQL datetime objects
+function convertJSDateToMySQLDate(date) {
+	return date.toISOString().slice(0, 10)
+}
 
 // a function to make a new appointment
 // possibly a new client (if client is a guest)
---
+++ src/app/test2/page.jsx
@@ -0,0 +1,9 @@
+import Test2Controller from "@/app/test2/Test2Controller";
+
+const page = () => {
+    return (
+        <Test2Controller/>
+    )
+}
+
+export default page;
