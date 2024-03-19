87c33762b5cf3acf26660c32d03f025a43d635de
Aryan Karadia <105018373+aryan-karadia@users.noreply.github.com>
2024-03-19 12:06:58 -0600

    Enhanced Browsing Filters and Category Display (#9)
    * Updated BrowsePets category button styles
    
    * trying to get browing filter to work
    
    * Added View all pets button
    
    * View specific pets category now works
    
    * Tweaking view all animal button
    
    ---------
    
    Co-authored-by: FDIL <fdil.fadilh1@gmail.com>

--- src/components/CategorySelectButton.jsx
+++ src/components/CategorySelectButton.jsx
@@ -1,9 +1,9 @@
 function CategorySelectButton({content, categoryState, newCategory, onClick}) {
 	return <button
-		className={`font-bold border-4 border-white ${categoryState === newCategory ? "bg-white" : "hover:bg-white"} text-black rounded-full px-6 py-3 mr-4`}
+		className={`font-bold border-4 border-white ${categoryState === newCategory ? "bg-white text-black" : "hover:bg-white hover:text-black"} text-white rounded-full px-6 py-3 mr-4`}
 		onClick={onClick}
 		disabled={categoryState === newCategory}
-		style={{backgroundColor: categoryState === newCategory ? "white" : ""}}
+		style={{backgroundColor: categoryState === newCategory ? "white" : "", color: categoryState === newCategory ? "black" : ""}}
 	>
 		{content}
 	</button>;
