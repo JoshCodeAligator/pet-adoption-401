40eb1632745fa863339bf5f0aab05274cc44d236
FDIL <fdil.fadilh1@gmail.com>
2024-03-20 16:58:16 -0600

    Update getBookedTimesOfWeek to use petID instead.
    Instead of centreID, make it use petID as that is what information Controller has.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/BookAppointment/BookAppointmentController.jsx
+++ src/app/BookAppointment/BookAppointmentController.jsx
@@ -1,9 +1,19 @@
 "use client"
 
-import React from 'react'
+import React, {useEffect} from 'react'
 import BookAppointmentView from "@/app/BookAppointment/BookAppointmentView";
+import {getBookedTimesOfWeek} from "@/app/BookAppointment/BookAppointmentAPI";
 
 const BookingController = ({pet_id}) => {
+
+
+    // upon loading of page, get all booked times for a centre and week starting from current date
+    useEffect(() => {
+        const getUnavailableTimesOfWeek = async () => {
+            const {} = await getBookedTimesOfWeek(new Date(), pet_id)
+        }
+    }, [pet_id]);
+
     return (
         <BookAppointmentView/>
     )
