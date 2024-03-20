c941178b8ce46fd3f7895461de4f3fc29fe116cd
FDIL <fdil.fadilh1@gmail.com>
2024-03-20 08:15:50 -0600

    Finish testing BookAppointmentAPI.js getBookedTimesOfWeek
    Works as intended, get array of date and start_time.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/BookAppointment/BookAppointmentAPI.js
+++ src/app/BookAppointment/BookAppointmentAPI.js
@@ -11,15 +11,17 @@ export async function getBookedTimesOfWeek(weekStartDate, centreID) {
 	let weekEndDate = new Date(weekStartDate)
 	weekEndDate.setDate(weekEndDate.getDate() + 6)
 
-
-	const startDate = convertJSDateToMySQLDate(weekStartDate)
-	const endDate = convertJSDateToMySQLDate(weekEndDate)
+	const [startDate, endDate] = await Promise.all([
+		convertJSDateToMySQLDate(weekStartDate),
+		convertJSDateToMySQLDate(weekEndDate)
+	])
 
 	console.log(startDate, endDate)
 
 	try {
 		const bookedTimesResult = await query(
-			'SELECT date, start_time FROM Appointment WHERE centre_id = ? AND (date BETWEEN ? AND ?)',
+			'SELECT date, start_time FROM Appointment WHERE centre_id = ? AND (date BETWEEN ? AND ?) ' +
+			'ORDER BY date, start_time ASC',
 			[centreID, startDate, endDate]
 		)
 
@@ -45,8 +47,12 @@ export async function getBookedTimesOfWeek(weekStartDate, centreID) {
 
 }
 
-// helper function to convert js date objects to MySQL datetime objects
-function convertJSDateToMySQLDate(date) {
+/**
+ * function to convert js date objects to MySQL date object
+ * @param date a js date object
+ * @return {Promise<string>}
+ */
+export async function convertJSDateToMySQLDate(date) {
 	return date.toISOString().slice(0, 10)
 }
 
