8be2556fab3fd6b9452b93b8a9dc7f496d17d1c8
FDIL <fdil.fadilh1@gmail.com>
2024-03-23 18:05:25 -0600

    Attempt at Appointment Booking logic.
    Not yet tested.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/BookAppointment/BookAppointmentView.jsx
+++ src/app/BookAppointment/BookAppointmentView.jsx
@@ -2,13 +2,13 @@ import Navbar from "@components/Navbar";
 import { useState } from "react";
 import Book from "./Book";
 
-const BookAppointmentView = ({unavailbleTimes, updateWeekStart}) => {
+const BookAppointmentView = ({unavailbleTimes, updateWeekStart, bookAppointment}) => {
   // State to keep track of the selected appointment type and animal type
 
   return (
     <div className="mx-auto max-w-screen-lg">
       <Navbar className="mt-10" />
-      <Book unavailableTimes={unavailbleTimes} updateStartDate={updateWeekStart}/>
+      <Book unavailableTimes={unavailbleTimes} updateStartDate={updateWeekStart} makeBooking={bookAppointment}/>
     </div>
   );
 };
