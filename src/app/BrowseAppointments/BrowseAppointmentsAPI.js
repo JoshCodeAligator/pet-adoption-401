ad9111bf1772fae14dba9d4056537497ea38eb54
FDIL <fdil.fadilh1@gmail.com>
2024-03-25 17:30:37 -0600

    SOme fixes to BrowseAppointmentsAPI.js
    Issue with the sql statement, fixed it.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/app/BrowseAppointments/BrowseAppointmentsAPI.js
+++ src/app/BrowseAppointments/BrowseAppointmentsAPI.js
@@ -5,11 +5,24 @@ import { getSessionUserID } from "@/lib";
 export async function getBookedAppointments(userId) {
     try {
         // Query the database to fetch booked appointments for the given user
-        console.log("userId: ", getSessionUserID)
-        const appointments = await query("SELECT * FROM Appointments WHERE userId = ?", [userId]);
+        console.log("userId: ", userId)
+        const appointments = await query("SELECT * FROM Appointments WHERE client_id = ?", [userId]);
 
         // Format the data as needed (e.g., convert date formats, sanitize data)
         // Example formatting: appointments.map(appointment => ({ id: appointment.id, date: formatDate(appointment.date), ... }));
+
+        // appointments will have the following form
+        // [
+        // {    appointment_id : int,
+        //     slot_no : int,
+        //     date : YYYY-MM-DD,
+        //     start_time : hh:mm:ss,
+        //     client_id int,
+        //     centre_id int,
+        //     pet_id int},
+        // {..}
+        //     ]
+        // each element of array is following json
 
         // Return the fetched appointments
         return {
