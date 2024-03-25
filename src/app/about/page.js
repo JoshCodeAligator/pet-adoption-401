bd577bb51258e83683f9585d653da74dc83a4b08
FDIL <fdil.fadilh1@gmail.com>
2024-03-24 18:14:21 -0600

    Update about page to ise NavBar instead of a copy implementation of old NavBar. This is to keep NavBar consistent between pages.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -59,28 +59,7 @@ const AboutPage = () => {
   return (
     <div className="min-h-screen bg-pawprint-pattern flex flex-col justify-center items-center py-12">
       {/* Navigation */}
-      <nav className="bg-black py-4 fixed top-0 left-0 w-full z-50 shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-          <div className="flex-shrink-0">
-            <a className="flex items-center text-white text-xl font-bold" href="/">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-8 w-8 mr-2">
-                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-              </svg>
-              <span>Home</span>
-            </a>
-          </div>
-          <div className="text-white text-lg lg:flex hidden">
-            <a className="ml-6" href="/about"><span>About</span></a>
-            <a className="ml-6" href="/client/CreateAccount"><span>Create Account</span></a>
-            <a className="ml-6" href="/login"><span>Log In</span></a>
-          </div>
-          <div className="lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-9 w-9 text-white mr-4 cursor-pointer">
-              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-            </svg>
-          </div>
-        </div>
-      </nav>
+      <Navbar/>
 
       {/* Main content */}
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
