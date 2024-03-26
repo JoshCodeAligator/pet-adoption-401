d5b6ef8ff089cecac2ecd86caad3d2dcebc3568c
Mohamed Ebdalla <mohamed.ebdalla@ucalgary.ca>
2024-03-26 02:15:58 -0600

    Appointments displayed, functionality works for each user

--- src/app/BrowseAppointments/BrowseAppointmentsView.jsx
+++ src/app/BrowseAppointments/BrowseAppointmentsView.jsx
@@ -8,7 +8,6 @@ import { getSessionUserID } from "@/lib";
 
 
 const BrowseAppointments = ({ appointments }) => {
-    console.log("Appointments2", appointments)
     return (
         <div>
             <Navbar /> 
@@ -19,6 +18,18 @@ const BrowseAppointments = ({ appointments }) => {
                     {/* Moves the image inside the left side div */}
                             <div className="w-80 h-80 relative">
                                 <h2> Your Appointments: {} </h2>
+                                {/* Render appointment details */}
+                            {appointments && appointments.map(appointment => (
+                                <div key={appointment.appointment_id}>
+                                    <p>Appointment ID: {appointment.appointment_id}</p>
+                                    <p>Date: {new Date(appointment.date).toLocaleDateString()}</p>
+                                    <p>Start Time: {appointment.start_time}</p>
+                                    <p>Client ID: {appointment.client_id}</p>
+                                    <p>Centre ID: {appointment.centre_id}</p>
+                                    {/* could get pet information from pet and display pets name and picture here */}
+                                    <p>Pet ID: {appointment.pet_id}</p>
+                                </div>
+                            ))}
                             </div>
                         </div>
                     </div>
