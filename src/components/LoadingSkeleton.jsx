4d14f7bf760e6d8d3256ed407cf6950c6bf48e61
FDIL <fdil.fadilh1@gmail.com>
2024-03-26 17:09:41 -0600

    Add suspense to BrowsePets page
    This is needed for building. Else build stops with an error.
    
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

---
+++ src/components/LoadingSkeleton.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@components/Navbar";
+import Background from "@components/Background";
+
+const LoadingSkeleton = () => {
+	return (
+		<>
+			<Navbar/>
+			<Background/>
+		</>
+	)
+}
+
+export default LoadingSkeleton
