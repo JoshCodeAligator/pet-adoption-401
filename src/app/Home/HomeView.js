4a63d253d15b740299d9ac8b386a128b120dae30
Aryan Karadia <karadiaaryan@gmail.com>
2024-03-25 19:48:44 -0600

    Add animated background to BrowsePetsView.jsx

--- src/app/Home/HomeView.js
+++ src/app/Home/HomeView.js
@@ -30,6 +30,7 @@ const HomeView = () => {
 
   return (
     <div>
+    <div>
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="background">
