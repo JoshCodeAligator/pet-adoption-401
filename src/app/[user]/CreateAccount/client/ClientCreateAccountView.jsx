9d78a19f418413177b2227aa1d1956db9095fa24
Aryan Karadia <karadiaaryan@gmail.com>
2024-03-25 20:42:40 -0600

    Add animated background to ClientCreateAccountView.jsx

--- src/app/[user]/CreateAccount/client/ClientCreateAccountView.jsx
+++ src/app/[user]/CreateAccount/client/ClientCreateAccountView.jsx
@@ -46,40 +46,7 @@ const ClientCreateAccountView = ({ onSubmit, errorFlag, errorMessage }) => {
 				{/*  end of animated background */}
 			</div>
             {/* CreateClientAccountForm in the second row */}
-            <div className="flex-grow flex flex-col justify-center items-center background">
-            {/* spans for the animated background */}
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+            <div className="flex-grow flex flex-col justify-center items-center">
             {/*  end of animated background */}
                 <div className="flex flex-col items-center">
                     <CreateClientAccountForm 
