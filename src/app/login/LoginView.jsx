12dc6aacd7baf0c36d05538c5aac08b38c16da2e
Aryan Karadia <karadiaaryan@gmail.com>
2024-03-25 20:44:05 -0600

    Add animated background to LoginView component

--- src/app/login/LoginView.jsx
+++ src/app/login/LoginView.jsx
@@ -8,6 +8,42 @@ const LoginView = ({onSubmit, errorFlag, errorMessage}) => {
 	return (
 		<>
 			<Navbar/>
+			<div className="background">
+				 {/* spans for the animated background */}
+				 <span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				{/*  end of animated background */}
+			</div>
 			<div className="h-screen flex justify-center items-center">
 				<LoginForm onSubmit={onSubmit}
 						   loginErrorMessage={errorMessage} loginErrorFlag={errorFlag}/>
