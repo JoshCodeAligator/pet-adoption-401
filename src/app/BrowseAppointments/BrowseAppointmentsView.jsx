77b0bee8996884a41f576b83a342f193785c33b3
Mohamed Ebdalla <mohamed.ebdalla@ucalgary.ca>
2024-03-26 15:56:18 -0600

    Cancel

--- src/app/BrowseAppointments/BrowseAppointmentsView.jsx
+++ src/app/BrowseAppointments/BrowseAppointmentsView.jsx
@@ -41,7 +41,7 @@ const BrowseAppointments = ({ appointments }) => {
                                         onClick={() => handleDelete(appointment.appointment_id)}
                                         className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                     >
-                                        Cancel Appointment
+                                        Cancel 
                                     </button>
                                 </div>
                             ))}
