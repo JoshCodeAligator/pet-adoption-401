300e97b51a964619334c23d98994982cd8f650da
Mohamed Ebdalla <mohamed.ebdalla@ucalgary.ca>
2024-03-26 17:36:54 -0600

    updated browseAppointments

---
+++ src/app/BrowseAppointments/BrowseAppointmentsController.js
@@ -0,0 +1,44 @@
+"use client"
+
+import React, { useState, useEffect } from "react";
+import { getBookedAppointments } from "@/app/BrowseAppointments/BrowseAppointmentsAPI";
+import { getSessionUserID } from "@/lib";
+import BrowseAppointmentsView from "./BrowseAppointmentsView";
+import getPetDetails from "../ViewPet/ViewPetAPI";
+
+const BrowseAppointmentsController = ()=> {
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        // Fetch booked appointments when the component mounts
+        const fetchAppointments = async () => {
+            try {
+                // Fetch the current user's session
+                const sessionUserID = getSessionUserID();
+    
+                // Fetch booked appointments for the current user
+                const response = await getBookedAppointments(sessionUserID);
+                
+                if (response) {
+                    // Update state with fetched appointments
+                    
+                    setAppointments(response);
+                    console.log("Response", response)
+                    console.log("Appointments", appointments)
+                } else {
+                    // Handle error
+                    console.error(response.error);
+                }
+            } catch (error) {
+                console.error("Error fetching appointments:", error);
+            }
+        };
+
+        fetchAppointments();
+
+    }, []);
+
+    return <BrowseAppointmentsView appointments={appointments} />;
+};
+
+export default BrowseAppointmentsController;
