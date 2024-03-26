fc7b2512bc77a0d0f479d7855849028029514abd
Aryan Karadia <karadiaaryan@gmail.com>
2024-03-25 19:48:57 -0600

    Refactor ClientCreateAccountView component

--- src/app/[user]/CreateAccount/client/ClientCreateAccountView.jsx
+++ src/app/[user]/CreateAccount/client/ClientCreateAccountView.jsx
@@ -5,18 +5,52 @@ import CreateClientAccountForm from "@CreateAccount/client/CreateClientAccountFo
 
 const ClientCreateAccountView = ({ onSubmit, errorFlag, errorMessage }) => {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col h-screen relative ">
             {/* Navbar in the first row */}
             <Navbar />
             
             {/* CreateClientAccountForm in the second row */}
-            <div className="flex-grow flex flex-col justify-center items-center">
+            <div className="flex-grow flex flex-col justify-center items-center background">
+            {/* spans for the animated background */}
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            {/*  end of animated background */}
                 <div className="flex flex-col items-center">
                     <CreateClientAccountForm 
                         onSubmit={onSubmit}
                         createErrorFlag={errorFlag}
                         createErrorMessage={errorMessage}
-                    />
+                        />
                 </div>
             </div>
         </div>
